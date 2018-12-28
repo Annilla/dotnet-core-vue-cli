ba3042c04042d3ca1fe47036b49622660d966633
Anny Chang <AnnyChang@kingstono365.onmicrosoft.com>
2018-12-28 16:22:22 +0800

    .NET Core Upgrade 2.2 with Webpack4 & Vue

---
+++ webpack.base.js
@@ -0,0 +1,63 @@
+const path = require('path')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const devMode = process.env.NODE_ENV === 'development'
+
+module.exports = {
+  entry: {
+    main: './src/main.js'
+  },
+  output: {
+    path: path.resolve(__dirname, './wwwroot'),
+    publicPath: '/wwwroot/',
+    filename: 'js/build.js'
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    },
+    extensions: ['*', '.js', '.vue', '.json']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss?$/,
+        use: [
+          devMode ? 'vue-style-loader' : MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2
+            }
+          },
+          'postcss-loader', // for autoprefix
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          publicPath: 'imgs/',
+          outputPath: 'imgs/',
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/main.css"
+    }),
+    new VueLoaderPlugin()
+  ]
+}
